61829e4f0de3b2d8fe3eb7345e0c39330c01d517
Mihail <mishavlashitschii@gmail.com>
2026-01-29 13:07:49 +0200

    TSK-31 [REFACTOR] Navigation and MVI pattern. Navigation logic should be only into the :app module    :app      * [REFACTOR] Rename YouTubeNavigation to BlueTubeNavigation      * [REFACTOR] Move all the navigation logic into thr BlueTubeBottomNavigation from the MVI implementations      * [ADD] BlueTubeNavigationTest.kt      * [REFACTOR] BlueTubeBottomNavigation.kt -> BackStack reuses and moves at the top of the stack same destinations rather than creating new once      * [REFACTOR] BlueTubeBottomNavigationTest.kt -> refactor and add tests      * [REFACTOR] Move the ScreenType.kt Navigation destinations contract from the :presentation_common into the :app module
       :presentation-player
         * [REFACTOR] PlayerMVI.kt -> move the Navigation logic into the :app module in BlueTubeNavigation.kt
         * [REFACTOR] PlayerMVITest.kt -> delete tests related to the navigation logic
    
       :presentation-common
         * [REFACTOR] build.gradle.kts -> delete Navigation 3 dependencies
    
       :test_common:ui_test
         *

--- app/src/main/java/com/vlascitchii/bluetubecompose/BlueTubeApp.kt
+++ app/src/main/java/com/vlascitchii/bluetubecompose/BlueTubeApp.kt
@@ -1,12 +1,12 @@
 package com.vlascitchii.bluetubecompose
 
 import androidx.compose.runtime.Composable
-import com.vlascitchii.bluetubecompose.navigation.YouTubeNavigation
+import com.vlascitchii.bluetubecompose.navigation.BlueTubeNavigation
 import com.vlascitchii.presentation_common.ui.theme.BlueTubeComposeTheme
 
 @Composable
 fun BlueTubeApp() {
     BlueTubeComposeTheme {
-        YouTubeNavigation()
+        BlueTubeNavigation()
     }
 }
--- presentation-common/src/main/java/com/vlascitchii/presentation_common/ui/video_list/VideoList.kt
+++ presentation-common/src/main/java/com/vlascitchii/presentation_common/ui/video_list/VideoList.kt
@@ -9,13 +9,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
